a94599f8b5f270b77eabb487d8938818ada1fe79
Ghack81 <herve-mention@hotmail.com>
2022-07-26 11:37:47 +0200

    Create db and first INSERT

--- db.sql
+++ db.sql
@@ -1,3 +1,4 @@
+/* Create a db for the test */
 CREATE TABLE IF NOT EXISTS `produit` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(256) NOT NULL,
@@ -8,7 +9,9 @@ CREATE TABLE IF NOT EXISTS `produit` (
 `modified` timestamp DEFAULT CURRENT_TIMESTAMP,
 primary key (id)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
+/*
+This is the first INSERT for test
 INSERT INTO produit VALUES (1, 'PEN', 'Pen Blue', 1.23, 1, '2020-7-04', '2020-9-03');
 INSERT INTO produit VALUES (2, 'PC', 'Apple products', 300.99, 2, '2019-1-01', '2019-5-14');
 INSERT INTO produit VALUES (3, 'Car', 'Mercedes benz', 985470.47, 3, '2019-2-06', '2019-6-23');
+*/
